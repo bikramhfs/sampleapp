c019c4d7275c2fe8f5f3736fe66fb9126e04811f
bikramhfs <bikram@headfitted.com>
2013-09-24 14:17:52 +0530

    completed 5th chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
 root to: 'static_pages#home'
 
-  match '/static_pages/baz', :to =>'static_pages#baz'
-  match '/static_pages/number', :to =>'static_pages#number'
+  match '/baz', :to =>'static_pages#baz'
+  match '/number', :to =>'static_pages#number'
   match '/home', :to =>'static_pages#home'
   match '/help', :to =>'static_pages#help'
   match '/about', :to =>'static_pages#about'
   match '/contact', :to =>'static_pages#contact'
+  match '/signup', :to =>'users#new'
  
 
  # get "foobars/number"
