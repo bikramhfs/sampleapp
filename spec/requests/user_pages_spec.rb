9660d21cfc229ba9e92a76402e7c1b588c428f14
bikramhfs <bikram@headfitted.com>
2013-09-30 11:52:00 +0530

    Finished user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,7 +23,7 @@ subject { page }
     #it { should have_selector('title', text: full_title('Sign up')) }
   end
   
-describe "signup" do
+ describe "signup" do
 
     before { visit signup_path }
 
